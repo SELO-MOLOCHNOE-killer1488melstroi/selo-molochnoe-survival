c2baed7d4092320e90a2365b803e30360ded4665
SELO-MOLOCHNOE-killer1488melstroi <vlad228aboba31@gmail.com>
2026-01-28 18:04:51 +0200

    dobavil kartinki and mellstroi and pesok and background asset and main file

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from pygame import *
 
 init()
 
-screen = display.set_mode((100,100))
+# ---------------- НАСТРОЙКИ ----------------
+WIDTH = 1366
+HEIGHT = 768
+FPS = 120
+
+# ---------------- ОКНО ----------------
+screen = display.set_mode((WIDTH, HEIGHT))
+display.set_caption("Jump Physics Demo")
+
+# ---------------- ФОН ----------------
+background = image.load("assets/background selo.png").convert()
+background = transform.scale(background, (WIDTH, HEIGHT))
 
 clock = time.Clock()
-
 running = True
 
-class Hero():
-    def __init__(self,x,y,image_path, speed):
+# ---------------- КЛАССЫ ----------------
+class Hero:
+    def __init__(self, x, y, image_path, speed):
         self.image = image.load(image_path).convert_alpha()
+        self.image = transform.scale(self.image, (200, 200))
+        self.rect = self.image.get_rect(midbottom=(x, y))
         self.speed = speed
         self.hp = 100
-        self.rect = self.image.get_rect(center=(x, y))
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
@@ -21,33 +33,80 @@ class Hero():
 
 class Player(Hero):
     def __init__(self, x, y):
-        super().__init__(x, y, "assets/mellstroi.png", 10)
+        super().__init__(x, y, "assets/mellstroi.png", 8)
+
+        # ФИЗИКА
+        self.vel_y = 0
+        self.gravity = 1
+        self.jump_power = -18
+        self.on_ground = False
 
     def move(self):
         keys = key.get_pressed()
 
+        # движение в стороны
         if keys[K_a]:
             self.rect.x -= self.speed
-
         if keys[K_d]:
             self.rect.x += self.speed
 
-        if keys[K_w]:
-            self.rect.y += self.speed
+        # прыжок
+        if keys[K_w] and self.on_ground:
+            self.vel_y = self.jump_power
+            self.on_ground = False
+
+        # гравитация
+        self.vel_y += self.gravity
+        self.rect.y += self.vel_y
+
+        # земля
+        ground = HEIGHT
+        if self.rect.bottom >= ground:
+            self.rect.bottom = ground
+            self.vel_y = 0
+            self.on_ground = True
+
+        # границы экрана
+        self.rect.clamp_ip(screen.get_rect())
+
 
 class Enemy(Hero):
     def __init__(self, x, y):
-        super().__init__(x, y, "assets/pesok.jng", 10)
+        super().__init__(x, y, "assets/enemy.png", 8)
 
-player = Player(100,100)
+        # ФИЗИКА
+        self.vel_y = 0
+        self.gravity = 1
+        self.jump_power = -18
+        self.on_ground = False
 
+    def move(self):
+        self.rect.x -= 1
+
+    def strike(self):
+        ...
+
+
+# ---------------- ИГРОК ----------------
+player = Player(WIDTH // 2, HEIGHT)
+
+enemy = Enemy(WIDTH // 2, HEIGHT)
+
+# ---------------- ЦИКЛ ИГРЫ ----------------
 while running:
     for e in event.get():
         if e.type == QUIT:
-            quit()
+            running = False
+
+    screen.blit(background, (0, 0))
+
+    enemy.move()
+    enemy.draw(screen)
 
     player.move()
     player.draw(screen)
 
     display.update()
-    clock.tick(60)
+    clock.tick(FPS)
+
+quit()
